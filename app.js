2834865fc3ef3777b7db5ff10d061c0540cc84a1
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 19:22:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Scope
+
+// var musica = 'Rock'
+// if (musica) {
+//  var musica = 'Grunge'
+//  console.log('Dentro del if: ', musica)
+// }
+// console.log('Fuera del if: ', musica)
+
+// Scope con let
+let musica = 'Rock'
+
+if (musica) {
+ let musica = 'Grunge'
+ console.log('Dentro del if: ', musica)
+}
+console.log('Fuera del if: ', musica)
